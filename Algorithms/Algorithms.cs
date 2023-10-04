1aa04edd61a77d0432d228206472a6fb5e22befe
keith <keith@wattcarbon.com>
2023-10-04 14:23:12 -0400

    algorithm and class refactoring finished

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,11 +1,33 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+       // private static Dictionary<int, int> Factorials = new Dictionary<int, int>() { {0,1}, {1, 1}};
+        public static int GetFactorial(int n) {
+            // TODO this will fail above n == 20
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            
+            var result = 1;
+            for (var i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+            return result;
+        }
+
+        public static string FormatSeparators(params string[] items)
+        {
+            // No Oxford comma
+            return  $"{string.Join(", ", items.SkipLast(1))} and {items.Last()}";
+        }
     }
 }
--- ClassRefactoring/ClassRefactor.cs
+++ ClassRefactoring/ClassRefactor.cs
@@ -9,22 +9,37 @@ namespace DeveloperSample.ClassRefactoring
 
     public enum SwallowLoad
     {
-        None, Coconut
+        None = 0, Coconut = 4
     }
 
     public class SwallowFactory
     {
-        public Swallow GetSwallow(SwallowType swallowType) => new Swallow(swallowType);
+        public Swallow GetSwallow(SwallowType swallowType)
+        {
+            switch (swallowType)
+            {
+                case SwallowType.European:
+                    return new EuropeanSwallow();
+                case SwallowType.African:
+                    return new AfricanSwallow();
+                default:
+                    return new Swallow(swallowType);
+            }
+        }
     }
 
     public class Swallow
     {
         public SwallowType Type { get; }
+        public readonly int AirspeedVelocity;
+        
         public SwallowLoad Load { get; private set; }
+        public int LoadAmount;
 
-        public Swallow(SwallowType swallowType)
+        public Swallow(SwallowType swallowType, int airspeedVelocity = 0) // This is to keep the airspeed readonly.
         {
             Type = swallowType;
+            AirspeedVelocity = airspeedVelocity;
         }
 
         public void ApplyLoad(SwallowLoad load)
@@ -34,23 +49,28 @@ namespace DeveloperSample.ClassRefactoring
 
         public double GetAirspeedVelocity()
         {
-            if (Type == SwallowType.African && Load == SwallowLoad.None)
+            var speed = AirspeedVelocity - (int)Load;
+            if (speed < 0)
             {
-                return 22;
+                throw new Exception("This swallow's airspeed is too low.");
             }
-            if (Type == SwallowType.African && Load == SwallowLoad.Coconut)
-            {
-                return 18;
-            }
-            if (Type == SwallowType.European && Load == SwallowLoad.None)
-            {
-                return 20;
-            }
-            if (Type == SwallowType.European && Load == SwallowLoad.Coconut)
-            {
-                return 16;
-            }
-            throw new InvalidOperationException();
+
+            return speed;
         }
+    }
+
+    public class AfricanSwallow : Swallow
+    {
+        public AfricanSwallow() : base(SwallowType.African, 22) 
+        {
+        }
+    }
+
+    public class EuropeanSwallow : Swallow
+    {
+        public EuropeanSwallow() : base(SwallowType.European, 20)
+        {
+        }
+
     }
 }
