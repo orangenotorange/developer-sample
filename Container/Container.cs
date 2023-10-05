14f4555621ffa37ce088b2660abf48449a748ae5
keith <keith@wattcarbon.com>
2023-10-04 20:28:08 -0400

    finished container test

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+        private  readonly Dictionary<Type, Type> _types = new();
+
+        public void Bind(Type interfaceType, Type implementationType)
+        {
+            _types.Add(interfaceType, implementationType);
+        }
+
+        public T Get<T>()
+        {
+            var hasValue = _types.TryGetValue(typeof(T), out var implementationType);
+            if (hasValue)
+            {
+                return (T)Activator.CreateInstance(implementationType);    
+            }
+
+            throw new Exception("No valid interface");
+        }
     }
 }
